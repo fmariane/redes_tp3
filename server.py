04281a7381426090111554d3926f10f45ca9f591
fmariane <f_mariane@hotmail.com>
2018-12-09 01:05:00 -0200

    implementado endpoint ix

--- server.py
+++ server.py
@@ -1,17 +1,54 @@
 from flask import Flask
+import json
 import requests
+import sys
 
-peeringdb = Flask(__name__)
+def parameterHandler():
+    parameter = {
+        "my_PORT": sys.argv[1],
+        "NETFILE": sys.argv[2],
+        "IXFILE": sys.argv[3],
+        "NETIXLANFILE": sys.argv[4],
+    }
+    return parameter
+
+mypeeringdb = Flask(__name__)
+
+# GET que recupera todos IXs ================================
+def get_ix():
+    req = requests.get('https://www.peeringdb.com/api/ix')
+    req_json = req.json()
+    return req_json
 
 
-@peeringdb.route('/')
-def get_exchange_points():
-    # endpoint /api/ix
-    return "exchange point"
+def get_ix_nets():
+    req = requests.get('https://www.peeringdb.com/api/net')
+    req_json = req.json()
+    return req_json
+
+def get_ix_netlan():
+    req = requests.get('https://www.peeringdb.com/api/netixlan')
+    req_json = req.json()
+    return req_json
+# ===========================================================
+parameters = parameterHandler()
+
+def load_json(file):
+    with open(file, "r") as jsonfile:
+        datastring = json.load(jsonfile)
+        datastring = json.dumps(datastring)
+    return datastring
 
 
-def exchangePointByID():
-    return "exchange point by id"
+#netfile = json.load(parameters["NETFILE"])
+#netixlanfile = json.load(parameters["NETIXLANFILE"])
 
+#endpoint /api/ix
+@mypeeringdb.route('/api/ix')
+def return_ix():
+    ixfile = load_json(parameters["IXFILE"])
+    return ixfile
 
-peeringdb.run(debug=True)
+def return_net
+
+mypeeringdb.run(debug=True)
