336543b0807c62698f07ab21427f743b34fcfd35
fmariane <f_mariane@hotmail.com>
2018-12-10 20:39:12 -0200

    server com tcp

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import json
+import socket
+import threading
 import sys
 from flask import Flask, jsonify
 
@@ -19,11 +21,26 @@ parameters = parameterHandler()
 mypeeringdb = Flask(__name__)
 
 
+# ========== TCP ROUTINES
+def tcp_handler():
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    HOST = ''                 # host sera a maquina que iniciar a aplicacao
+    PORT = int( sys.argv[1] ) # Porta que o servidor TCP deve ficar
+    orig = (HOST, PORT)
+    tcp.bind(orig)
+    tcp.listen(1)
+
+    while True:
+        client_connection, address = tcp.accept()
+
+# ========== FLASK ROUTINES
 def load_json(file):
     with open(file, "r") as jsonfile:
         datastring = json.load(jsonfile)  # json to dict
         datastring = json.dumps(datastring)  # dump string
     return datastring
+
 
 # endpoint /api/ix
 @mypeeringdb.route('/api/ix')
@@ -74,5 +91,8 @@ def return_net_name(netid):
     responsedata = {'data':ix_nets[netid]}
     return jsonify(responsedata)
 
-PORT = int(parameters["my_PORT"])
-mypeeringdb.run(host='0.0.0.0', port=PORT, debug=True)
+
+t = threading.Thread(target=tcp_handler)  # starta primeiro o servidor tcp
+t.daemon = True
+t.start()
+mypeeringdb.run(host='0.0.0.0', port=None, debug=None)  # servidor flask usa outra interface disponivel
