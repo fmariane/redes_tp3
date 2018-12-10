813465525a397d874809bc2bc02a7c1a2137b6e7
fmariane <f_mariane@hotmail.com>
2018-12-10 12:59:40 -0200

    implementacao do endpoint ixnets

--- client.py
+++ client.py
@@ -1,8 +1,35 @@
 import json
 import socket
+import struct
 import sys
 
-server_IP = sys.argv[1]
-server_PORT = sys.argv[2]
-
 # chamada do programa./client IP:port Opt
+
+IP_PORT = sys.argv[1]
+OPT = sys.argv[2]
+
+ip_port_tuple = IP_PORT.split(':')
+server_IP = ip_port_tuple[0]
+server_PORT = ip_port_tuple[1]
+dest = (server_IP, int(server_PORT))
+
+try:
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        tcp.connect(dest)
+    except socket.error:
+        print("erro connect tcp")
+        # sys.stderr.write("erro connect tcp")
+
+except socket.error:
+    print("erro criacao do socket")
+    # sys.stderr.write("erro criacao do socket")
+
+
+def analytics(IX, NET, NETIXLAN):
+    IX = NET = NETIXLAN = ''
+    return 'results'
+
+
+resposta = tcp.recv(2048)
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask
 import json
-import requests
 import sys
+import requests
+from flask import Flask
+
 
 def parameterHandler():
     parameter = {
@@ -12,43 +13,42 @@ def parameterHandler():
     }
     return parameter
 
+
+parameters = parameterHandler()
+
+
 mypeeringdb = Flask(__name__)
 
-# GET que recupera todos IXs ================================
-def get_ix():
-    req = requests.get('https://www.peeringdb.com/api/ix')
-    req_json = req.json()
-    return req_json
-
-
-def get_ix_nets():
-    req = requests.get('https://www.peeringdb.com/api/net')
-    req_json = req.json()
-    return req_json
-
-def get_ix_netlan():
-    req = requests.get('https://www.peeringdb.com/api/netixlan')
-    req_json = req.json()
-    return req_json
-# ===========================================================
-parameters = parameterHandler()
 
 def load_json(file):
     with open(file, "r") as jsonfile:
-        datastring = json.load(jsonfile)
-        datastring = json.dumps(datastring)
+        datastring = json.load(jsonfile)  # json to dict
+        datastring = json.dumps(datastring)  # dump string
     return datastring
 
 
-#netfile = json.load(parameters["NETFILE"])
-#netixlanfile = json.load(parameters["NETIXLANFILE"])
+# netfile = json.load(parameters["NETFILE"])
+# netixlanfile = json.load(parameters["NETIXLANFILE"])
 
-#endpoint /api/ix
+# endpoint /api/ix
 @mypeeringdb.route('/api/ix')
 def return_ix():
     ixfile = load_json(parameters["IXFILE"])
     return ixfile
 
-def return_net
 
-mypeeringdb.run(debug=True)
+# print('ESTE É O RETORO ix', return_ix())
+
+
+@mypeeringdb.route('/api/ixnets/<int:ix_id>')
+def return_net(ix_id):
+
+    # real_peeringdb_url = 'https://www.peeringdb.com/api/net/'+str(ix_id)
+    # ixnetsbyid = requests.get(url=real_peeringdb_url).json()
+    # return json.dumps(ixnetsbyid, indent=4)
+
+
+# print('ESTE E O RETORNO NET', json.dumps(return_net(3), indent=4))
+
+PORT = int(parameters["my_PORT"])
+mypeeringdb.run(host='0.0.0.0', port=PORT, debug=True)
